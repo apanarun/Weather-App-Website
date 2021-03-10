a58486c6c8f564bbb482e321579796507b8a8dfd
Arun A <apanarun@gmail.com>
2021-03-10 13:43:03 +0530

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log('Client Side Javascript file')
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message1')
